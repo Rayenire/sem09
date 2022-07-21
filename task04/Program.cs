90544af2431752859549d0722628f45a562db8a9
Rayenire <aeristarg@gmail.com>
2022-07-21 11:24:19 +0300

    Add task04

---
+++ task04/Program.cs
@@ -0,0 +1,10 @@
+﻿int f(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return f(a, b - 1) * a;   
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write(f(a, b));
